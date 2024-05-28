0fecdf0b075f143374b710990df7541d5245ae4f
Los Espartanos <166185806+20espartanos24@users.noreply.github.com>
2024-05-28 17:20:34 -0500

    Add files via upload

---
+++ Hidedelegate_Despues.py
@@ -0,0 +1,21 @@
+class Departamento:
+    def __init__(self, nombre, gerente):
+        self.nombre = nombre
+        self.gerente = gerente
+
+    def obtener_gerente(self):
+        return self.gerente
+
+
+class Empleado:
+    def __init__(self, nombre, departamento):
+        self.nombre = nombre
+        self._departamento = departamento
+
+    def obtener_gerente(self):
+        return self._departamento.obtener_gerente()
+    
+departamento = Departamento("RRHH", "Sr. Hurtado")
+empleado = Empleado("Paolo Sosa", departamento)
+
+print(empleado.obtener_gerente())  # Salida: Sr. Hurtado
